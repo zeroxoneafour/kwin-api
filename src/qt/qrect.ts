990706f57fe7eb0e39b95d712405fa4fb7d9cd32
Vaughan Milliman <vaughan@vaughanm.xyz>
2026-07-25 13:08:43 -0500

    nvm ig

--- src/qt/qrect.ts
+++ src/qt/qrect.ts
@@ -4,8 +4,12 @@ export interface QRect {
     width: number;
     height: number;
 
+    // KWIN API DOES NOT SUPPORT THESE!
+    // Idk why either
+    /*
     readonly left: number;
     readonly right: number;
     readonly top: number;
     readonly bottom: number;
+    */
 }
